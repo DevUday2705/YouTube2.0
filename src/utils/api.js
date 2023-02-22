15fada0e24f406a595c850e18d34e87904c39e27
devuday2705 <uday@qwickbit.com>
2023-02-23 00:29:04 +0530

    Updated Api

--- src/utils/api.js
+++ src/utils/api.js
@@ -3,15 +3,14 @@ import axios from "axios";
 const BASE_URL = "https://youtube138.p.rapidapi.com";
 
 const options = {
-    params: { hl: "en", gl: "US" },
-    headers: {
-        "X-RapidAPI-Key":
-            process.env.REACT_APP_YOUTUBE_API_KEY || "YOUR_API_KEY",
-        "X-RapidAPI-Host": "youtube138.p.rapidapi.com",
-    },
+  params: { hl: "en", gl: "US" },
+  headers: {
+    "X-RapidAPI-Key": "a23233e11dmsh4ad6d3afebd36dcp117aa4jsna698b22f5a19",
+    "X-RapidAPI-Host": "youtube138.p.rapidapi.com",
+  },
 };
 
 export const fetchDataFromApi = async (url) => {
-    const { data } = await axios.get(`${BASE_URL}/${url}`, options);
-    return data;
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+  return data;
 };
